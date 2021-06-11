0f3c3fa7670ba58431460ae381e3bc544ec81b47
NotThorz <reda45696@gmail.com>
2021-06-11 02:57:01 +0000

    existing files

---
+++ src/components/Languages/Languages.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import Card from "react-bootstrap/Card";
+import CardDeck from "react-bootstrap/CardDeck"
+
+const Languages = () => {
+    return (
+        <div>
+    <h1 className="pt-3 text-center font-details-b pb-3">Languages</h1>
+    <CardDeck>
+  <Card>
+    <Card.Img variant="top" src="./english.jpg" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+</CardDeck>
+  <br />
+        </div>
+    )
+}
+
+export default Languages
